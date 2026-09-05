4443f0588c846f3ad133d7eb965bafcb3adf2a5e
WebMav <294071804+Webmav@users.noreply.github.com>
2026-09-06 00:01:52 +0530

    Made question one and two and made start to function as hide detail getting and ask a question and recieve an answer

---
+++ resource/script.js
@@ -0,0 +1,44 @@
+//first
+const inputName = document.querySelector("#nGSubmitName");
+const inputGmail = document.querySelector("#nGSubmitGmail");
+const startButton = document.querySelector("#startButton");
+
+const firstDiv = document.querySelector('#first');
+const secondDiv = document.querySelector('#second');
+const thirdDiv = document.querySelector("#third");
+const fourthDiv = document.querySelector("#fourth");
+const fifthDiv = document.querySelector("#fifth");
+const sixthDiv = document.querySelector("#sixth");
+
+let name1;
+let gmail1;
+
+secondDiv.style.display = "none";
+thirdDiv.style.display = "none";
+fourthDiv.style.display = "none";
+fifthDiv.style.display = "none";
+sixthDiv.style.display = "none";
+
+startButton.addEventListener('click',()=>{
+    name1 = inputName.value;
+    gmail1 = inputGmail.value;
+
+    firstDiv.style.display = "none";
+
+    let math = Math.floor(Math.random() * 100);
+    if(math<=20){
+        secondDiv.style.display = "";
+    }
+    else if(math<=40){
+        thirdDiv.style.display = "";
+    }
+    else if(math<=60){
+        fourthDiv.style.display = "";
+    }
+    else if(math<=80){
+        fifthDiv.style.display = "";
+    }
+    else if(math<=100){
+        sixthDiv.style.display = "";
+    }
+})
