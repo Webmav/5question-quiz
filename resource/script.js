d8a95720cb42ceb4d3e6d3e79ba47be0bf5a1acd
WebMav <294071804+Webmav@users.noreply.github.com>
2026-09-06 00:36:00 +0530

    Made next button function so that it generates  the five questions randomly

--- resource/script.js
+++ resource/script.js
@@ -2,7 +2,8 @@
 const inputName = document.querySelector("#nGSubmitName");
 const inputGmail = document.querySelector("#nGSubmitGmail");
 const startButton = document.querySelector("#startButton");
-
+const nextButtons = document.querySelectorAll(".next");
+ 
 const firstDiv = document.querySelector('#first');
 const secondDiv = document.querySelector('#second');
 const thirdDiv = document.querySelector("#third");
@@ -12,12 +13,14 @@ const sixthDiv = document.querySelector("#sixth");
 
 let name1;
 let gmail1;
+let maths;
 
 secondDiv.style.display = "none";
 thirdDiv.style.display = "none";
 fourthDiv.style.display = "none";
 fifthDiv.style.display = "none";
 sixthDiv.style.display = "none";
+
 
 startButton.addEventListener('click',()=>{
     name1 = inputName.value;
@@ -26,6 +29,7 @@ startButton.addEventListener('click',()=>{
     firstDiv.style.display = "none";
 
     let math = Math.floor(Math.random() * 100);
+    maths = math;
     if(math<=20){
         secondDiv.style.display = "";
     }
@@ -41,4 +45,102 @@ startButton.addEventListener('click',()=>{
     else if(math<=100){
         sixthDiv.style.display = "";
     }
+    console.log(maths); 
+})
+
+
+nextButtons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        if(maths<=20){
+            secondDiv.style.display = "none";
+            
+            let math = Math.floor(Math.random() * 100);
+            maths = math;
+            if(math<=40){
+                thirdDiv.style.display = "";
+            }
+            else if(math<=60){
+                fourthDiv.style.display = "";
+            }
+            else if(math<=80){
+                fifthDiv.style.display = "";
+            }
+            else if(math<=100){
+                sixthDiv.style.display = "";
+            }
+        }
+        else if(maths<=40){
+            thirdDiv.style.display = "none";
+
+            let math = Math.floor(Math.random() * 100);
+            maths = math;
+            if(math<=20){
+                secondDiv.style.display = "";
+            }
+            else if(math<=60){
+                fourthDiv.style.display = "";
+            }
+            else if(math<=80){
+                fifthDiv.style.display = "";
+            }
+            else if(math<=100){
+                sixthDiv.style.display = "";
+            }
+        }
+        else if(maths<=60){
+            fourthDiv.style.display = "none";
+
+            let math = Math.floor(Math.random() * 100);
+            maths = math;
+            if(math<=20){
+                secondDiv.style.display = "";
+            }
+            else if(math<=40){
+                thirdDiv.style.display = "";
+            }
+            else if(math<=80){
+                fifthDiv.style.display = "";
+            }
+            else if(math<=100){
+                sixthDiv.style.display = "";
+            }
+        }
+        else if(maths<=80){
+            fifthDiv.style.display = "none";
+
+            let math = Math.floor(Math.random() * 100);
+            maths = math;
+            if(math<=20){
+                secondDiv.style.display = "";
+            }
+            else if(math<=40){
+                thirdDiv.style.display = "";
+            }
+            else if(math<=60){
+                fourthDiv.style.display = "";
+            }
+            else if(math<=100){
+                sixthDiv.style.display = "";
+            }
+        }
+        else if(maths<=100){
+            fifthDiv.style.display = "none";
+
+            let math = Math.floor(Math.random() * 100);
+            maths = math;
+            if(math<=20){
+                secondDiv.style.display = "";
+            }
+            else if(math<=40){
+                thirdDiv.style.display = "";
+            }
+            else if(math<=60){
+                fourthDiv.style.display = "";
+            }
+            else if(math<=80){
+                fifthDiv.style.display = "";
+            }
+        }
+    })
+    console.log(maths);
 })
