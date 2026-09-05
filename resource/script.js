519d41cbb99a5d5509ea808421b198846ae7ea86
WebMav <294071804+Webmav@users.noreply.github.com>
2026-09-06 03:58:19 +0530

    Made so that pressing enter functions as clicking button

--- resource/script.js
+++ resource/script.js
@@ -129,8 +129,99 @@ nextButtons.forEach((button)=>{
     })
 })
 
-//rem
-//click enter for next or button
+//remaining
+
 //accept real name
 //accept real gmail
 //make footer
+//View answers (right or wrong)
+
+document.addEventListener('keypress',(event)=>{
+    key = event.key;
+    if(key == 'Enter'){
+        console.log(key);
+//details page
+        if(details.style.display == ""){
+            name1 = inputName.value;
+            gmail1 = inputGmail.value;
+
+            if((name1 == "") || (gmail1 == "")){
+                alert("Fill in all the details");
+            }
+            else {
+                details.style.display = "none";
+
+                first.style.display = "";
+            }
+        }
+//other pages
+        else {
+            if((ans1 == "") || (ans1 == undefined)){
+                ans1 = qFirst.value ;
+                if(ans1 == ""){
+                    alert("Enter ANSWER!!!")
+                }
+                else{
+                    first.remove();
+                    second.style.display = '';
+                        if((ans1 == "delhi") || (ans1 == "Delhi")){
+                            scores += 20;
+                        }
+                }
+            }
+            else if((ans2 == "") || (ans2 == undefined)){
+                ans2 = qSecond.value ;
+                if(ans2 == ""){
+                    alert("Enter ANSWER!!!")
+                }
+                else{
+                    second.remove();
+                    third.style.display = '';
+                        if((ans2 == "jupiter") || (ans2 == "Jupiter")){
+                            scores += 20;
+                        }
+                }
+            }
+            else if((ans3 == "") || (ans3 == undefined)){
+                ans3 = qThird.value ;
+                if(ans3 == ""){
+                    alert("Enter ANSWER!!!")
+                }
+                else{
+                    third.remove();
+                    fourth.style.display = '';
+                        if((ans3 == "paris") || (ans3 == "Paris")){
+                            scores += 20;
+                        }
+                }
+            }
+            else if((ans4 == "") || (ans4 == undefined)){
+                ans4 = qFourth.value ;
+                if(ans4 == ""){
+                    alert("Enter ANSWER!!!")
+                }
+                else{
+                    fourth.remove();
+                    fifth.style.display = '';
+                        if((ans4 == "green") || (ans4 == "Green")){
+                            scores += 20;
+                        }
+                }
+            }
+            else if((ans5 == "") || (ans5 == undefined)){
+                ans5 = qFifth.value ;
+                if(ans5 == ""){
+                    alert("Enter ANSWER!!!")
+                }
+                else{
+                    fifth.remove();
+                    score.style.display = '';
+                        if(ans5 == "366"){
+                            scores += 20;
+                        }
+                    scoress.textContent = `${scores} / 100`;
+                }
+            }
+        }
+    }
+})
